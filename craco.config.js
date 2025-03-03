5f72509a966c4c88bdb03b24762b0d207b0e4146
MacBookPro <macbookpro@MacBook-Pro-von-MacBookPro.local>
2025-03-03 11:01:45 +0100

    Aktualisiere den Simulations Ordner

--- craco.config.js
+++ craco.config.js
@@ -2,7 +2,6 @@
 module.exports = {
   webpack: {
     configure: (webpackConfig) => {
-      // Füge eine Regel hinzu, die source-map-loader in node_modules ignoriert
       webpackConfig.module.rules = webpackConfig.module.rules.map(rule => {
         if (rule.use) {
           rule.use = rule.use.map(u => {
@@ -12,7 +11,6 @@ module.exports = {
                 options: {
                   ...u.options,
                   filterSourceMappingUrl: (url, resourcePath) => {
-                    // Ignoriere Source Maps aus node_modules
                     return resourcePath.includes('node_modules');
                   },
                 },
